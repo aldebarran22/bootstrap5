a9b6f131da83c086ebb90ebd4d0198ec63249ba4
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-03-09 14:02:40 +0100

    g1_jueves_9

---
+++ grupo1/portafolio_final/app.js
@@ -0,0 +1,25 @@
+// Código JS para abrir la ventana
+
+// Asignar una función flecha de JS al evento load (salta cuando está cargada la página 
+// en el navegador )
+window.onload = () => {
+
+    // Capturamos la ventana modal:
+    const imagenModal = document.getElementById('imagen-modal')
+    console.log('src de la ventana modal'+ imagenModal.src)
+
+    // Seleccionar todos los contenedores con imagen a partir de la clase: contenedor-img 
+    const imagenes = document.querySelectorAll('.contenedor-img')
+    console.log('Numero de contenedores: '+ imagenes.length)
+
+    // Recorrer el array de contenedores:
+    imagenes.forEach((imagen)=>{
+        // Añadir evento click a cada uno de los contenedores de la imagen
+        imagen.addEventListener('click', ()=>{
+            // Captura la imagen que tiene cada contenedor y copia la ruta a la ventana modal
+            const pathImagen = imagen.querySelector('img').src
+            //console.log('Copiar la ruta : ' + pathImagen)
+            imagenModal.src = pathImagen
+        })
+    })
+}
